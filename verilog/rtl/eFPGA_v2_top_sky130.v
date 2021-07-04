f9fe0641e597b95b9836a73283ef7e391414dd66
Nguyen Dao <nguyen.dao@manchester.ac.uk>
2021-07-04 22:04:34 +0100

    update gds

--- verilog/rtl/eFPGA_v2_top_sky130.v
+++ verilog/rtl/eFPGA_v2_top_sky130.v
@@ -1,4 +1,6 @@
-// SPDX-FileCopyright 2021 Nguyen Dao
+// SPDX-FileCopyrightText: 
+// 2020 Efabless Corporation
+// 2021 Nguyen Dao
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
